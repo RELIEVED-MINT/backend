15bba1e548a72274c4d6ee1e3b84e0a1924ea3ac
Stephen Wong <wongstephenky@gmail.com>
2022-08-27 12:06:30 -0700

    added history delete route

--- controllers/historyController.js
+++ controllers/historyController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
+//create
 router.post("/", (req, res, next) => {
   const historyData = req.body;
   const userId = historyData.owner;
@@ -11,6 +12,19 @@ router.post("/", (req, res, next) => {
       return user.save();
     })
     .then((user) => res.status(201).json({ user: user }))
+    .catch(next);
+});
+
+//destroy
+// delete /history/:id
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  User.findOne({ "reviews._id": id })
+    .then((user) => {
+      user.history.id(id).remove();
+      return user.save();
+    })
+    .then(() => res.sendStatus(204))
     .catch(next);
 });
 
